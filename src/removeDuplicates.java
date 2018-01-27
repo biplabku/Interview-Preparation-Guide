a300d2b6061db69db01e8dc1aff8a2ddf570af1f
Biplab Kumar Das <dasbiplabtu@gmail.com>
2018-01-26 19:37:14 -0600

    Feat - Bug fix.

--- src/removeDuplicates.java
+++ src/removeDuplicates.java
@@ -1,12 +1,21 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+
 public class removeDuplicates {
 
 
-    public static int[] getRemoveDuplicates(int[] array) {
-        
+    public static HashSet<Integer> removeDuplicates(int[] nums) {
+        HashSet<Integer> hset = new HashSet<>();
+        for(int j = 0; j < nums.length; j++) {
+            if(!hset.contains(nums[j])) {
+                hset.add(nums[j]);
+            }
+        }
+        return hset;
     }
 
     public static void main(String[] args) {
-        int[] array = new int[] {};
-        System.out.println(getRemoveDuplicates(array));
+        int[] array = new int[] {1,1,2,2,8,6,3};
+        System.out.println(removeDuplicates(array));
     }
 }
