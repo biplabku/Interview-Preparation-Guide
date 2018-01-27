e99859f53bba8d3c635cbfb160ec85ab24c8d84e
Biplab Kumar Das <dasbiplabtu@gmail.com>
2018-01-27 10:18:42 -0600

    Feat - Bug fix. Updated the optimized solution

--- src/moveZeros.java
+++ src/moveZeros.java
@@ -2,7 +2,9 @@ import java.util.LinkedList;
 
 public class moveZeros {
 
-    public static LinkedList<Integer> moveZeros(int[] array) {
+    public static int[] array = new int[] {0,1,0,3,12};
+
+    public static LinkedList<Integer> moveZeros() {
         LinkedList<Integer> list = new LinkedList<>();
         for(int i  = 0; i < array.length; i++) {
             if(array[i] != 0) {
@@ -17,9 +19,33 @@ public class moveZeros {
         return list;
     }
 
+    // time complexity - O(N). space complexity - O(1). In place
+    public static void moveZerosMethod1() {
+        int pointer1= 0;
+        for(int i = 0; i < array.length; i++) {
+            if(array[i] != 0) {
+               swap(pointer1, i);
+               pointer1 = pointer1 + 1;
+            }
+        }
+        display();
+    }
+
+    public static void swap(int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+
+    public static void display() {
+        for(int i = 0; i < array.length;i ++) {
+            System.out.print(array[i]+ " ");
+        }
+    }
     public static void main(String[] args) {
-        LinkedList<Integer> list = new LinkedList<>();
-        int[] array = new int[] {1,2,3,4,};
-        System.out.println(moveZeros(array));
+        // System.out.println(moveZeros());
+        moveZerosMethod1();
+
     }
 }
