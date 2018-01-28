eb2e0dcaed15900629e034309ca8c80bd05dc617
Biplab Kumar Das <dasbiplabtu@gmail.com>
2018-01-27 19:29:11 -0600

    Feat - Bugfix. Updated the code

--- src/buyAndSell.java
+++ src/buyAndSell.java
@@ -4,29 +4,23 @@ public class buyAndSell {
 
 
     public static int findMaxStock(int[] array) {
-        ArrayList<Integer> list= new ArrayList<>();
-        int min = array[0];
-        int ptr1 = 0;
-        int ptr2 = 1;
-        int max = array[1];
-        int diff = 0;
-        for(int i = 0;i < array.length-1; i++) {
-            if(min > array[i]) {
-                ptr1  = i;
-                min = Math.min(min, array[i]);
-            }
-            if(max < array[i + 1]) {
-                ptr2 = i + 1;
-                max = Math.max(max, array[i + 1]);
-            }
-            diff = Math.max((max - min), diff);
+        if(array.length == 0) {
+            return 0;
         }
-
-        return diff;
+        int max = 0;
+        int curMax = array[0];
+        for(int i = 0; i < array.length; i++) {
+            if(array[i] > curMax) {
+                max = Math.max(max, array[i] - curMax);
+            }else {
+                curMax = array[i];
+            }
+        }
+        return max;
     }
 
     public static void main(String[] args) {
-        int[] array  = new int[] {7, 5, 4, 3, 2, 1};
+        int[] array  = new int[] {5,4,3,2,1};
         System.out.println(findMaxStock(array));
     }
 }
