5cb0a88eaa7a0e777d971c472b34bc0e4b731db8
Biplab Kumar Das <dasbiplabtu@gmail.com>
2018-01-28 21:55:50 -0600

    Feat - Bug fix. Created LRU cache from the theory

--- src/LRUCache.java
+++ src/LRUCache.java
@@ -1,46 +1,73 @@
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Queue;
 import java.util.Stack;
 
 public class LRUCache {
 
-    public int capacity ;
-    public Stack<Integer> theStack = new Stack<>();
-    public HashMap<Integer, Integer> hmap = new HashMap<>();
+    int capacity;
+    HashMap<Integer, Node> map = new HashMap<>();
+    Node head=null;
+    Node end=null;
 
-    public LRUCache(int val) {
-        capacity = val;
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
     }
 
-    public int getElement(int key) {
-        if(hmap.containsKey(key)) {
-            return hmap.get(key);
-        }else {
-            return -1;
+    public int get(int key) {
+        if(map.containsKey(key)){
+            Node node = map.get(key);
+            remove(node);
+            setHead(node);
+            return node.value;
         }
+
+        return -1;
     }
 
-    public int insertElement(int key, int val) {
-        if(!hmap.containsKey(key) && hmap.size() != capacity) {
-            hmap.put(key, val);
-            theStack.push(key);
-        }else if(hmap.size() == capacity) {
-            hmap.remove(theStack.pop());
-            hmap.put(key, val);
-            theStack.push(key);
+    public void remove(Node n){
+        if(n.pre!=null){
+            n.pre.next = n.next;
+        }else{
+            head = n.next;
         }
-        return theStack.peek();
+
+        if(n.next!=null){
+            n.next.pre = n.pre;
+        }else{
+            end = n.pre;
+        }
+
     }
 
-    public static void main(String[] args) {
-        LRUCache ls = new LRUCache(3);
-        ls.insertElement(1,12);
-        ls.insertElement(2,22);
-        ls.insertElement(3,32);
-        System.out.println(ls.getElement(1));
-        ls.insertElement(4, 44);
-        System.out.println(ls.getElement(3));
+    public void setHead(Node n){
+        n.next = head;
+        n.pre = null;
 
+        if(head!=null)
+            head.pre = n;
+        head = n;
+        if(end ==null)
+            end = head;
+    }
 
+    public void set(int key, int value) {
+        if(map.containsKey(key)){
+            Node old = map.get(key);
+            old.value = value;
+            remove(old);
+            setHead(old);
+        }else{
+            Node created = new Node(key, value);
+            if(map.size()>=capacity){
+                map.remove(end.key);
+                remove(end);
+                setHead(created);
+
+            }else{
+                setHead(created);
+            }
+            map.put(key, created);
+        }
     }
 }
