1a7e5a37440dbad8359a4aead4881e4a558623f5
Biplab Kumar Das <dasbiplabtu@gmail.com>
2018-01-29 21:07:53 -0600

    Feat - New Feature. Adding first unrepeated code

---
+++ src/firstUnrepeated.java
@@ -0,0 +1,13 @@
+public class firstUnrepeated {
+
+
+    public static int findUnrepeatedChar(String str) {
+        
+    }
+
+    public static void main(String[] args) {
+
+        String str = "";
+        System.out.println(findUnrepeatedChar(str));
+    }
+}
