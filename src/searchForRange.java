42fa63e26242cad192dd505857b7f719e6b1c1a6
Biplab Kumar Das <dasbiplabtu@gmail.com>
2018-02-03 10:32:57 -0600

    Feat - New Feature. Adding code for searchForRange and some bug fix

--- src/searchForRange.java
+++ src/searchForRange.java
@@ -9,14 +9,15 @@ public class searchForRange {
         int low = 1;
         int high = nums.length;
         while(low <= high) {
-            int mid = low + (high - low) /2;
+            int mid = (low + (high - low)) /2;
             if(nums[mid] == target) {
-                list.add(low, high);
+                list.add(low);
+                list.add( high);
                 return list;
             }else if(nums[mid] < target) {
-                high = mid +1;
+                low = mid +1;
             }else {
-                low = mid - 1;
+                high = mid - 1;
             }
         }
         list.add(-1);
@@ -24,10 +25,42 @@ public class searchForRange {
         return list;
     }
 
+    public static ArrayList<Integer> searhRangeMethod2(int[] array, int target) {
+        int low = -1;
+        int high = -1;
+
+        for(int i = 0; i < array.length; i++) {
+            if(array[i] == target) {
+                low = i;
+                break;
+            }
+        }
+
+        for(int  i = array.length - 1; i >= 0; i--) {
+            if(array[i] == target) {
+                high = i;
+                break;
+            }
+        }
+
+        ArrayList<Integer> list= new ArrayList<>();
+        list.add(low);
+        list.add(high);
+        return list;
+    }
+
+    public static ArrayList<Integer> searchRangeMethod3(int[] array, int target) {
+        ArrayList<Integer> list= new ArrayList<>();
+        list.add(-1);
+        list.add(-1);
+        int low = getFirstMatch(array, target);
+    }
+
+
     public static void main(String[] args) {
        int[] array = new int[] {5, 7, 7, 8, 8, 10} ;
        int target = 8;
 
-       System.out.println(searchRange(array, target));
+       System.out.println(searhRangeMethod2(array, target));
     }
 }
