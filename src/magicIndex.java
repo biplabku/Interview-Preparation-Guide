c71116e9121867b8e9713c2ebf36b410d27e80a7
Biplab Kumar Das <dasbiplabtu@gmail.com>
2018-02-04 10:17:03 -0600

    Feat - Bug fix. Adding improvement for repeated elements in the array

--- src/magicIndex.java
+++ src/magicIndex.java
@@ -12,7 +12,7 @@ public class magicIndex {
     }
 
     public static int getMagic(int[] array) {
-        return getMagicIndexFast(array, 0, array.length -1);
+        return getMagicIndexRepeat(array, 0, array.length -1);
     }
 
     public static int getMagicIndexFast( int[] array, int low, int high) {
@@ -27,6 +27,26 @@ public class magicIndex {
         }else {
             return getMagicIndexFast(array, mid + 1, high);
         }
+    }
+
+
+    public static int getMagicIndexRepeat(int[] array, int low, int high) {
+        if(low > high) {
+            return  -1;
+        }
+        int midIndex = (low + high)/2;
+        int midValue = array[midIndex];
+        if(array[midIndex ]  == midValue) {
+            return midIndex;
+        }
+        int leftIndex = Math.min(midIndex - 1, midValue);
+        int left = getMagicIndexRepeat(array, low, leftIndex) ;
+        if(left >= 0) {
+            return left;
+        }
+        int rightIndex = Math.max(midIndex + 1, midValue);
+        int right = getMagicIndexRepeat(array, rightIndex, high);
+        return right;
     }
 
     public static void main(String[] args) {
