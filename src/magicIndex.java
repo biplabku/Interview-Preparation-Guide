a81167949e5c3f900d74e0be7d9e99d86999eac3
Biplab Kumar Das <dasbiplabtu@gmail.com>
2018-02-04 09:57:32 -0600

    Feat - Bug fix. Adding faster implementation using binary search

--- src/magicIndex.java
+++ src/magicIndex.java
@@ -11,8 +11,26 @@ public class magicIndex {
         return -1;
     }
 
+    public static int getMagic(int[] array) {
+        return getMagicIndexFast(array, 0, array.length -1);
+    }
+
+    public static int getMagicIndexFast( int[] array, int low, int high) {
+        if(low > high) {
+            return -1;
+        }
+        int mid = (low + high)/2;
+        if(array[mid] == mid) {
+            return mid;
+        }else if(array[mid] > mid) {
+            return getMagicIndexFast(array, low, mid - 1);
+        }else {
+            return getMagicIndexFast(array, mid + 1, high);
+        }
+    }
+
     public static void main(String[] args) {
-        int[] array = new int[] {1,2,4,6,7,9,11};
-        System.out.println(getMagicIndex(array));
+        int[] array = new int[] {-40, -20, -1, 1,2,3,5,7,9,12,13 };
+        System.out.println(getMagic(array));
     }
 }
