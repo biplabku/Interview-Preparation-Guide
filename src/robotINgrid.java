a03b614355c03491d51487278ef703286ed4a35e
Biplab Kumar Das <dasbiplabtu@gmail.com>
2018-02-04 09:33:03 -0600

    Feat - Bug fix. Adding a faster implementation

--- src/robotINgrid.java
+++ src/robotINgrid.java
@@ -1,5 +1,6 @@
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class robotINgrid {
 
@@ -30,7 +31,34 @@ public class robotINgrid {
     }
 
 
-    
+    public static ArrayList<Point> getPath1(boolean[][] maze) {
+        if(maze == null || maze.length == 0) {
+            return null;
+        }
+        ArrayList<Point> path = new ArrayList<>();
+        HashSet<Point> failedPoints = new HashSet<>();
+        if(getPath1(maze, maze[0].length, maze.length, path, failedPoints)) {
+            return path;
+        }
+        return null;
+    }
+
+    public static boolean getPath1(boolean[][] maze, int row, int col, ArrayList<Point> path, HashSet<Point> failedPoints) {
+        if(row < 0 || col < 0 || !maze[row][col]) {
+            return false;
+        }
+        Point p = new Point(row, col) ;
+        if(failedPoints.contains(p)) {
+            return false;
+        }
+
+        boolean isAtOrigin  = (row == 0) && (col == 0);
+        if((getPath1(maze, row - 1, col, path, failedPoints) || getPath1(maze, row, col -1, path, failedPoints) || isAtOrigin)){
+            path.add(p);
+            return true;
+        }
+        return false;
+    }
 
     public static void main(String[] args) {
         boolean[][] array = new boolean[5][5];
