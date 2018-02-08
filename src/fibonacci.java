9218cdc3805bd6893a93e1e921e9092b9c49e744
Biplab Kumar Das <dasbiplabtu@gmail.com>
2018-02-07 20:06:13 -0600

    Feat - Update. Updating the first code

--- src/fibonacci.java
+++ src/fibonacci.java
@@ -3,12 +3,15 @@ public class fibonacci {
 
 
     public static int getFibonaciMethod1(int number) {
-
+        if(number == 0 || number == 1) {
+            return 1;
+        }
+        return getFibonaciMethod1(number - 1)  + getFibonaciMethod1(number - 2);
     }
 
     public static void main(String[] args) {
 
         int number = 9;
-        System.out.println(getFibonaciMethod1(number));
+        System.out.println(getFibonaciMethod1(5));
     }
 }
