1c5854ef96f40ccf0b20fe55c5c11213dc9cf0f6
Biplab Kumar Das <dasbiplabtu@gmail.com>
2018-02-07 20:25:00 -0600

    Feat - Update. Adding optimized code

--- src/fibonacci.java
+++ src/fibonacci.java
@@ -9,9 +9,24 @@ public class fibonacci {
         return getFibonaciMethod1(number - 1)  + getFibonaciMethod1(number - 2);
     }
 
+    public static int getFibonaciMethod2(int number) {
+        if(number == 0 || number == 1) {
+            return 1;
+        }
+        int[] fib = new int[number + 1];
+        fib[0] = 1;
+        fib[1] = 1;
+        for(int i = 2; i <= number; i++) {
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+        return fib[number];
+    }
+
+
     public static void main(String[] args) {
 
-        int number = 9;
-        System.out.println(getFibonaciMethod1(5));
+        int number = 5;
+        System.out.println(getFibonaciMethod1(number));
+        System.out.println(getFibonaciMethod2(number));
     }
 }
