9a6dd6f7dd64aab9bf5313436826b05436a17eb6
Biplab Kumar Das <dasbiplabtu@gmail.com>
2018-02-09 00:58:11 -0600

    Feat - Update. Adding working code

--- src/powerOfTwo.java
+++ src/powerOfTwo.java
@@ -1,7 +1,24 @@
 public class powerOfTwo {
 
+
+    public static boolean getPowerOfTwo(int number) {
+        if(number == 1 || number == 2) {
+            return true;
+        }
+        int prev = 1;
+        int out = 1;
+        while(prev < number) {
+            out = 2 * prev;
+            prev = out;
+            if(out == number) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
-        int number = 4;
-        
+        int number = 2147483647s;
+        System.out.println(getPowerOfTwo(number));
     }
 }
