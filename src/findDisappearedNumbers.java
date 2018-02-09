b5fc7387c6793d38a61cf4ac6d8dcd5064b2fffc
Biplab Kumar Das <dasbiplabtu@gmail.com>
2018-02-08 23:49:03 -0600

    Feat - Update. Adding an optimized solution

--- src/findDisappearedNumbers.java
+++ src/findDisappearedNumbers.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class findDisappearedNumbers {
     // Given an array of integers where 1 ≤ a[i] ≤ n (n = size of array),
@@ -8,9 +9,7 @@ public class findDisappearedNumbers {
     // Find all the elements of [1, n] inclusive that do not appear in
     // this array.
     public static ArrayList<Integer> findDisappearedNumbers(int[] array) {
-        //
         ArrayList<Integer> list = new ArrayList<>();
-        int n = array.length;
         for(int i = 0 ;i < array.length; i++) {
             if(!list.contains(array[i])) {
                 list.add(array[i]);
@@ -25,10 +24,26 @@ public class findDisappearedNumbers {
         return list1;
     }
 
-    
+    public static ArrayList<Integer> findDisappearNumber(int[] array) {
+        ArrayList<Integer> list = new ArrayList<>();
+        HashMap<Integer, Integer> hmap = new HashMap<>();
+        for(int i = 0 ;i < array.length; i++) {
+            if(!hmap.containsKey(array[i])) {
+                hmap.put(array[i], i);
+            }
+        }
+        for(int  i = 1; i <= array.length; i++) {
+            if(!hmap.containsKey(i)) {
+                list.add(i);
+                System.out.println(list) ;
+            }
+        }
+        return list;
+    }
 
     public static void main(String[] args) {
         int[] arr = new int[]{1,1};
-        System.out.println(findDisappearedNumbers(arr));
+        //System.out.println(findDisappearedNumbers(arr));
+        System.out.println(findDisappearNumber(arr));
     }
 }
