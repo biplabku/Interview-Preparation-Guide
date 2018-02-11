9c88c35c7ba3a12fc4d52f9a3e938e0ce4e61c96
Biplab Kumar Das <dasbiplabtu@gmail.com>
2018-02-10 18:14:47 -0600

    Feat - Update. Adding improved code for plus one

--- src/plusOne.java
+++ src/plusOne.java
@@ -3,7 +3,7 @@ import java.util.LinkedList;
 
 public class plusOne {
 
-    public static int[] getPlusOne(int[] digits) {
+    public static LinkedList<Integer> getPlusOne(int[] digits) {
         LinkedList<Integer> list  = new LinkedList<>();
         int add = 1;
         int sum = 0;
@@ -21,12 +21,26 @@ public class plusOne {
         if(carry > 0) {
             list.addFirst(carry);
         }
-        return list.toArray();
+        return list;
+    }
+
+    public static int[] getPlusOneMethod1 (int[] digits) {
+        for(int i = digits.length - 1; i >= 0 ; i--) {
+            if(digits[i] < 9) {
+                digits[i] = digits[i] + 1;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] out = new int[digits.length + 1];
+        out[0] = 1;
+        return  out;
     }
 
 
     public static void main(String[] args) {
         int [] digit = new int[] {9,9,9} ; // 1 -- 2 -- 3
         System.out.println(getPlusOne(digit));
+        System.out.println(getPlusOneMethod1(digit));
     }
 }
