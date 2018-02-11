ed1fe66ba544e7235affb9d490031fa622d1a29d
Biplab Kumar Das <dasbiplabtu@gmail.com>
2018-02-11 16:12:03 -0600

    Feat - Update. Adding a faster implmentation

--- src/validAnagram.java
+++ src/validAnagram.java
@@ -38,11 +38,28 @@ public class validAnagram {
         return true;
     }
 
+    public static boolean isValidMethod2(String s, String t) {
+        if(s.length() != t.length()) {
+            return false;
+        }
+        int[] count = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0)
+                return false;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         String s = "anagram";
         String t = "nagaram";
         System.out.println(isValidAnagram(s, t));
         System.out.println(isValidMethod1(s, t));
+        System.out.println(isValidMethod2(s, t));
 
     }
 }
