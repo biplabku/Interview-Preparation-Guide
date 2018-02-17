e71d7450de642c99a0b51bd65788bde999d3b160
Biplab Kumar Das <dasbiplabtu@gmail.com>
2018-02-16 19:38:19 -0600

    Feat - Update. Adding new Implementation for remove duplicates

--- src/removeDuplicates.java
+++ src/removeDuplicates.java
@@ -27,8 +27,30 @@ public class removeDuplicates {
         return hset;
     }
 
+    public static ArrayList<Integer> removeDuplicates2(ArrayList<Integer> nums) {
+        HashSet<Integer> hset = new HashSet<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int j = 0; j < nums.size(); j++) {
+            if(!hset.contains(nums.get(j))) {
+                hset.add(nums.get(j));
+                list.add(nums.get(j));
+
+            }
+        }
+        return list;
+    }
+
     public static void main(String[] args) {
         int[] array = new int[] {1,1,2,2,8,6,3};
-        System.out.println(removeDuplicates(array));
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(1);
+        list.add(2);
+        list.add(2);
+        list.add(8);
+        list.add(6);
+        list.add(3);
+        // System.out.println(removeDuplicates(array));
+        System.out.println(removeDuplicates2(list));
     }
 }
