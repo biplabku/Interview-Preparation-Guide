7a7ddd6727a8911c9694a750f4d650d6710ccf13
Biplab Kumar Das <dasbiplabtu@gmail.com>
2018-02-17 11:29:17 -0600

    Feat - Update. Adding the  final implementation

--- src/isMatch.java
+++ src/isMatch.java
@@ -22,6 +22,30 @@ public class isMatch {
         return (count1 == 0 && count2 == 0);
     }
 
+        public boolean isMatch1(String s, String p) {
+
+            if(p.length() == 0)
+                return s.length() == 0;
+            // condition when there is no match
+            if(p.length() == 1 || p.charAt(1) != '*'){
+                if(s.length() < 1 || (p.charAt(0) != '.' && s.charAt(0) != p.charAt(0)))
+                    return false;
+                return isMatch(s.substring(1), p.substring(1));
+
+            }else{
+                int len = s.length();
+
+                int i = -1;
+                while(i<len && (i < 0 || p.charAt(0) == '.' || p.charAt(0) == s.charAt(i))){
+                    if(isMatch(s.substring(i+1), p.substring(2)))
+                        return true;
+                    i++;
+                }
+                return false;
+            }
+        }
+
+
     public static void main(String[] args) {
         String str1 = "";
         String str2 = ".*";
