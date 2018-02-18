eb2b381404024cb6c10ba3193f89af09cb000ed1
Biplab Kumar Das <dasbiplabtu@gmail.com>
2018-02-18 17:20:00 -0600

    Feat - New Feature. Adding evenOdd implmentation

---
+++ src/evenOdd.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class evenOdd {
+
+
+    public static int[] setTheElements(int[] array) {
+        int nextEven = 0;
+        int nextOdd = array.length - 1;
+        while(nextEven < nextOdd) {
+            if(array[nextEven] %2 == 0 ) {
+                nextEven++ ;
+            }else {
+                int temp = array[nextEven];
+                array[nextEven] = array[nextOdd];
+                array[nextOdd] = temp;
+                nextOdd--;
+            }
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[] {1,2,3,4,5,6,7};
+        System.out.println(setTheElements(array));
+    }
+}
