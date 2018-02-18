e93daae88595bf599bc59002c30dd1cdbde79e80
Biplab Kumar Das <dasbiplabtu@gmail.com>
2018-02-17 22:15:03 -0600

    Feat - Update. Implemented intersect list code

--- src/intersectList.java
+++ src/intersectList.java
@@ -1,5 +1,17 @@
 public class intersectList {
 
+    public static LinkNode findIntersectNode(LinkNode node1, LinkNode node2) {
+        LinkNode cur = null;
+        while(node1 != null && node2 != null) {
+            cur = node1;
+            if(node1 == node2) {
+                return cur;
+            }
+            node1 = node1.next;
+            node2 = node2.next;
+        }
+        return null;
+    }
 
     public static void main(String[] args) {
 
