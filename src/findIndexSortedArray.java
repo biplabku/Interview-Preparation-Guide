a01a17cce6a2bf78a9ab245ae5c5705e88e214c9
Biplab Kumar Das <dasbiplabtu@gmail.com>
2018-02-19 20:08:47 -0600

    Feat - New Feature. Adding implementation for findIndex sorted array

---
+++ src/findIndexSortedArray.java
@@ -0,0 +1,12 @@
+public class findIndexSortedArray {
+
+
+    public static int findIndexSortedArray(int[] array) {
+        
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[] {4,5,6,7,0,1,2};
+        System.out.println(findIndexSortedArray());
+    }
+}
