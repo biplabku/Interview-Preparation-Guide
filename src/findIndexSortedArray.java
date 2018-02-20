c40863ea40e429170af9fd690e92e875bec69a46
Biplab Kumar Das <dasbiplabtu@gmail.com>
2018-02-19 20:36:04 -0600

    Feat - Update. Adding a final implementation of finding the index of the target in a sorted array

--- src/findIndexSortedArray.java
+++ src/findIndexSortedArray.java
@@ -11,12 +11,32 @@ public class findIndexSortedArray {
         }
         findIndexSortedArray(array, leftIndex, mid, target) ;
         findIndexSortedArray(array, mid+1, rightIndex, target);
+        return -1;
     }
 
+    public static int findIndexArray(int[] array, int leftIndex, int rightIndex, int target) {
+        while (leftIndex < rightIndex) {
+            int mid = (leftIndex + rightIndex) / 2;
+            if (array[mid] > array[rightIndex]) {  // eg. 3,4,5,6,1,2
+                if (target > array[mid] || target <= array[rightIndex]) {
+                    leftIndex = mid + 1;
+                } else {
+                    rightIndex = mid;
+                }
+            } else {  // eg. 5,6,1,2,3,4
+                if (target > array[mid] && target <= array[rightIndex]) {
+                    leftIndex = mid + 1;
+                } else {
+                    rightIndex = mid;
+                }
+            }
+        }
+        if (leftIndex == rightIndex && target != array[leftIndex]) return -1;
+        return leftIndex;
+    }
 
-    
     public static void main(String[] args) {
         int[] array = new int[] {4,5,6,7,0,1,2};
-        System.out.println(findIndexSortedArray(array, 0, array.length-1, 1));
+        System.out.println(findIndexArray(array, 0, array.length-1, 1));
     }
 }
