a37431edd170ff58bffd4f0054e0eb6269e055db
Biplab Kumar Das <dasbiplabtu@gmail.com>
2018-02-20 19:43:32 -0600

    Feat - Update. Adding a naive implementation for longest palindrome substring in O(N3)

--- src/longestPalindrome.java
+++ src/longestPalindrome.java
@@ -1,8 +1,30 @@
 public class longestPalindrome {
-    
+
+
+    public static String getLongestPalindromeSubstring(String str) {
+        String res = "";
+        for(int i =0; i < str.length(); i++) {
+            for(int j = i+1; j < str.length(); j++) {
+                if(isPalindrome(str, i, j) && (res.length() < (j - i))) {
+                    res = str.substring(i, j);
+                }
+            }
+        }
+        return res;
+    }
+
+    public static boolean isPalindrome(String str, int left, int right) {
+        for(int i = left; i < right; i++) {
+            if(str.charAt(i) != str.charAt(right - i - 1)){
+                return false;
+            }
+        }
+        return true;
+    }
 
     public static void main(String[] args) {
-        String str = "babab";
-        System.out.println(getLongestSubstring(str));
+        String str = "bababa";
+        System.out.println(getLongestPalindromeSubstring(str));
+        //System.out.println(isPalindrome(str, 0, 5));
     }
 }
