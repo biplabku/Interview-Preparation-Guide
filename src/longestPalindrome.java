eab205663e05a667931f7101defede1e0ffa8fc6
Biplab Kumar Das <dasbiplabtu@gmail.com>
2018-02-20 20:57:52 -0600

    Feat - Update. Adding efficient space complexity solution

--- src/longestPalindrome.java
+++ src/longestPalindrome.java
@@ -72,6 +72,42 @@ public class longestPalindrome {
         return longest;
     }
 
+
+    public static String longestPalindromeE(String s) {
+        if (s.isEmpty()) {
+            return null;
+        }
+
+        if (s.length() == 1) {
+            return s;
+        }
+
+        String longest = s.substring(0, 1);
+        for (int i = 0; i < s.length(); i++) {
+            // get longest palindrome with center of i
+            String tmp = helper(s, i, i);
+            if (tmp.length() > longest.length()) {
+                longest = tmp;
+            }
+
+            // get longest palindrome with center of i, i+1
+            tmp = helper(s, i, i + 1);
+            if (tmp.length() > longest.length()) {
+                longest = tmp;
+            }
+        }
+
+        return longest;
+    }
+
+    public static String helper(String s, int begin, int end) {
+        while (begin >= 0 && end <= s.length() - 1 && s.charAt(begin) == s.charAt(end)) {
+            begin--;
+            end++;
+        }
+        return s.substring(begin + 1, end);
+    }
+
     public static void main(String[] args) {
         String str = "babd";
         // System.out.println(getLongestPalindromeSubstring(str));
@@ -79,6 +115,6 @@ public class longestPalindrome {
 
         //System.out.println(isPalindrome1(str));
         // System.out.println(getLongestPalindromeSubstringMethod2(str));
-        System.out.println(longestPalindrome(str));
+        System.out.println(longestPalindromeE(str));
     }
 }
