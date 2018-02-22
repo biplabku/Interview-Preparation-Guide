f41032da5370c985f2a4058ee1274206885bf635
Biplab Kumar Das <dasbiplabtu@gmail.com>
2018-02-21 23:08:33 -0600

    Feat - Update. Adding another implmenetation with same time and space complexity

--- src/productOfArray.java
+++ src/productOfArray.java
@@ -22,8 +22,38 @@ public class productOfArray {
         return list;
     }
 
+    public static int[] getProductsOfAllIntsExceptAtIndex(int[] intArray) {
+
+        if (intArray.length < 2) {
+            throw new IllegalArgumentException("Getting the product of numbers at other indices requires at least 2 numbers");
+        }
+
+        int[] productsOfAllIntsExceptAtIndex = new int[intArray.length];
+
+
+        int productSoFar = 1;
+        for (int i = 0; i < intArray.length; i++) {
+            productsOfAllIntsExceptAtIndex[i] = productSoFar;
+            productSoFar *= intArray[i];
+        }
+
+        // for each integer, we find the product of all the integers
+        // after it. since each index in products already has the
+        // product of all the integers before it, now we're storing
+        // the total product of all other integers
+        productSoFar = 1;
+        for (int i = intArray.length - 1; i >= 0; i--) {
+            productsOfAllIntsExceptAtIndex[i] *= productSoFar;
+            productSoFar *= intArray[i];
+        }
+
+        return productsOfAllIntsExceptAtIndex;
+    }
+
     public static void main(String[] args) {
         int[] array = new int[] {1,7,0,4};
         System.out.println(getProductOfArray(array));
+        System.out.println(getProductsOfAllIntsExceptAtIndex(array));
+
     }
 }
