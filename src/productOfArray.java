c9a5d5a1ffaabe5856fbfa4f6747e03bd2f65751
Biplab Kumar Das <dasbiplabtu@gmail.com>
2018-02-21 22:50:48 -0600

    Feat - Update. Adding bug fix for handling the condition when the array contains zeros

--- src/productOfArray.java
+++ src/productOfArray.java
@@ -4,18 +4,26 @@ public class productOfArray {
 
     public static ArrayList<Integer> getProductOfArray(int[] array) {
         int mul = 1;
-        for(int i = 0; i < array.length; i++) {
-            mul = mul * array[i];
+        for(int i = 0; i < array.length; i++) { // O(N) // O(1)
+            if(array[i] == 0 ) {
+                mul = mul * 1;
+            }else {
+                mul = mul * array[i];
+            }
         }
         ArrayList<Integer> list = new ArrayList<>();
-        for(int i = 0; i < array.length; i++) {
-            list.add(mul/array[i]);
+        for(int i = 0; i < array.length; i++) {  // O(N) // O(N)
+            if(array[i] == 0) {
+                list.add(mul/1);
+            }else{
+                list.add(mul/array[i]);
+            }
         }
         return list;
     }
 
     public static void main(String[] args) {
-        int[] array = new int[] {1,7,3,4};
+        int[] array = new int[] {1,7,0,4};
         System.out.println(getProductOfArray(array));
     }
 }
