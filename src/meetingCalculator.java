ecedf763c37d7df3a4d0f86323610bde15e252fc
Biplab Kumar Das <dasbiplabtu@gmail.com>
2018-02-26 21:05:12 -0600

    Feat - Update. Adding class description and initialization

--- src/meetingCalculator.java
+++ src/meetingCalculator.java
@@ -1,5 +1,12 @@
 public class meetingCalculator {
 
     public static void main(String[] args) {
+        meeting m1 = new meeting(1,2);
+        meeting m2 = new meeting(3,4);
+        meeting m3 = new meeting(4,5);
+        meeting m4 = new meeting(8,9);
+
+        meeting[] array = new meeting[] {m1, m2,m3, m4};
+        
     }
 }
--- src/meetingTime.java
+++ src/meetingTime.java
@@ -1,4 +1,12 @@
 public class meetingTime {
+
+    // Your company built an in-house calendar tool called HiCal. You want to add a feature to see the
+    // times in a day when everyone is available.
+
+    // To do this, you’ll need to know when any team is having a meeting. In HiCal, a meeting is stored
+    // as an object of a Meeting class with integer variables startTime and endTime. These integers represent
+    // the number of 30-minute blocks past 9:00am.
+
     private int startTime;
     private int endTime;
 
@@ -26,7 +34,7 @@ public class meetingTime {
 
 
     public static void main(String[] args) {
-        
+
     }
 
 }
