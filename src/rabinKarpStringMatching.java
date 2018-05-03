2d34db4ed6abedac12589a7522f879dd9157d049
Biplab Kumar Das <dasbiplabtu@gmail.com>
2018-05-02 22:34:31 -0500

    Feat - Update. Adding loop implementation

--- src/rabinKarpStringMatching.java
+++ src/rabinKarpStringMatching.java
@@ -4,15 +4,17 @@ public class rabinKarpStringMatching {
     public boolean NaiveSearch(String str, String pattern) {
         for(int i = 0; i < (str.length() - pattern.length() + 1); i++) {
             for(int j = 0; j < pattern.length(); j++) {
-                
+                if(str.substring(j, i + j -1) == pattern.substring(j)) {
+                    return true;
+                }
             }
         }
         return false;
     }
 
     public static void main(String[] args) {
-        String str = "";
-        String pattern = "";
+        String str = "pattern";
+        String pattern = "ern";
         rabinKarpStringMatching rs = new rabinKarpStringMatching();
         System.out.println(rs.NaiveSearch(str, pattern));
     }
