183c02954e78d3da5260b23fb8c38bd8e40994ac
Biplab Kumar Das <dasbiplabtu@gmail.com>
2018-05-02 22:15:05 -0500

    Feat - New Feature. Adding string matching implmentation

---
+++ src/rabinKarpStringMatching.java
@@ -0,0 +1,14 @@
+public class rabinKarpStringMatching {
+
+
+    public boolean NaiveSearch(String str, String pattern) {
+        return false;
+    }
+
+    public static void main(String[] args) {
+        String str = "";
+        String pattern = "";
+        rabinKarpStringMatching rs = new rabinKarpStringMatching();
+        System.out.println(rs.NaiveSearch(str, pattern));
+    }
+}
