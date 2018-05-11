a859cd3908d87ea25abddb024b63f70361bbf57e
Biplab Kumar Das <dasbiplabtu@gmail.com>
2018-05-10 19:40:06 -0500

    Feat - Update. Saving the uncommited changes

--- src/heapSort.java
+++ src/heapSort.java
@@ -2,7 +2,11 @@ public class HeapSort {
 
   private int[] heapArray;
   private int size;
-  
+
+  public HeapSort(int val) {
+    heapArray = new int[val];
+    
+  }
 
 
   public static void main(String[] args) {
