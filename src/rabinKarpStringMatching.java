835738b2f3ea790f211a445d682a07ea6e59cdd9
Biplab Kumar Das <dasbiplabtu@gmail.com>
2018-07-10 19:37:30 -0500

    Feat - New Feature. Adding Rabin Karp initial implementation

--- src/rabinKarpStringMatching.java
+++ src/rabinKarpStringMatching.java
@@ -13,6 +13,7 @@ public class rabinKarpStringMatching {
     }
 
     public int BruteForceAlgo(String text, String pattern) {
+        if(pattern.length() > text.length()) return -1;
         for(int i = 0; i < (text.length() - pattern.length() + 1); i++) {
             int j = 0;
             while(j < pattern.length() && (text.charAt(i + j) == pattern.charAt(j))) {
@@ -25,11 +26,12 @@ public class rabinKarpStringMatching {
         return -1;
     }
 
+
     public static void main(String[] args) {
         String str = "pattern";
         String pattern = "ern";
         rabinKarpStringMatching rs = new rabinKarpStringMatching();
         // System.out.println(rs.NaiveSearch(str, pattern));
-        System.out.println(rs.BruteForceAlgo("abababcdababababd", "abababab"));
+        System.out.println(rs.BruteForceAlgo("abababcdababababd", "abababcdababababd"));
     }
 }
