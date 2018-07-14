74186f2a11053b9c8c04eec5836792f147b08ec2
Biplab Kumar Das <dasbiplabtu@gmail.com>
2018-07-14 16:52:47 -0500

    Feat - Update. Adding final implementation for KMP algorithm

--- src/KMPAlgo.java
+++ src/KMPAlgo.java
@@ -2,11 +2,31 @@ public class KMPAlgo {
 
 
     // Knuth morris pratt algorithm
-    public static boolean KMPalgo(String text, String pattern) {
-
+    public  boolean KMPalgo(String txt, String ptr) {
+        char[] text = txt.toCharArray();
+        char[] pattern = ptr.toCharArray();
+        int i = 0;
+        int j = 0;
+        int[] lps = computeArray(pattern);
+        while(i < txt.length() && j < ptr.length()) {
+            if(text[i] == pattern[j]) {
+                i++;
+                j++;
+            }else {
+                if(j != 0) {
+                    j = lps[j - 1];
+                }else {
+                    i++;
+                }
+            }
+        }
+        if(j == pattern.length) {
+            return true;
+        }
+        return false;
     }
 
-    public int[] computeArray(char[] pattern) {
+    public  int[] computeArray(char[] pattern) {
         int[] lps = new int[pattern.length];
         int index = 0;
         for(int i = 1; i < pattern.length;) {
@@ -30,5 +50,8 @@ public class KMPAlgo {
 
     public static void main(String[] args) {
         KMPAlgo kmp = new KMPAlgo();
+        String str = "abcxabcdabcdabcy";
+        String ptr = "abcdabcs";
+        System.out.println(kmp.KMPalgo(str, ptr));
     }
 }
