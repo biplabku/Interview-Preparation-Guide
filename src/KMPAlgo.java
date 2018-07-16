fc93b75899160060395b37cc136104d514d3ea96
Biplab Kumar Das <dasbiplabtu@gmail.com>
2018-07-15 21:28:48 -0500

    Feat - Update. Adding initial implementation rough

--- src/KMPAlgo.java
+++ src/KMPAlgo.java
@@ -27,12 +27,53 @@ public class KMPAlgo {
     // Since it gives how many times the character or String needs to be repeated
     // I can use recursion here
     public String compressDecompress(String inputString) {
-        for(int i = 0; i < inputString.length(); i++) {
-            if(Character.isDigit(inputString.charAt(i))) {
-                System.out.println(inputString.charAt(i));
+        String[] arr = inputString.split("]");
+        String str = "3[abc]4[ab]c";
+        String[] arr1 = str.split("]");
+        System.out.println(arr1[0] +  " " + arr1[1] + " " + arr1[2]);
+        System.out.println(arr[0] + " " + arr[1]);
+        StringBuilder sb = new StringBuilder();
+        for(int i = arr1.length - 1; i >= 0; i--) {
+            String s = arr1[i];
+            // get the number and then append the string as many times
+            int count = 0;
+            while(Character.isDigit(s.charAt(i))) {
+                count++;
             }
+
         }
         return "Done";
+    }
+
+    public String doCompress(String str) {
+        StringBuilder sb1 = new StringBuilder();
+        int len = str.length() - 1;
+        while(len != 1) {
+            StringBuilder sb = new StringBuilder();
+            while(str.charAt(len) != '[') {
+                sb.append(str.charAt(len));
+                len = len - 1;
+            }
+            System.out.println(len);
+            int right = len;
+            int left = len;
+            while(len != 0 && Character.isDigit(str.charAt(len))) {
+                left = len;
+
+                len = len - 1;
+            }
+            String countStr = str.substring(left, right);
+
+            System.out.println(countStr  + " Stromg" + left  + " " + right);
+
+            String temp = sb.toString();
+            /*
+            while(count != 0) {
+                sb1.append(sb.toString());
+            }
+            */
+        }
+        return sb1.toString();
     }
 
     // Knuth morris pratt algorithm
@@ -89,6 +130,7 @@ public class KMPAlgo {
         // System.out.println(kmp.KMPalgo(str, ptr));
         String str1 = "3[abs]4[ab]c";
         String str2 = "2[3[a]b]";
-        System.out.println(kmp.compressDecompress(str2));
+        // System.out.println(kmp.compressDecompress(str2));
+        System.out.println("The compress value -- > " + kmp.doCompress("3[abc"));
     }
 }
