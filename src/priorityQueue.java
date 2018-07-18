f6ddf897eed58cd6202f5b2bd03f9b902cd7b3ce
Biplab Kumar Das <dasbiplabtu@gmail.com>
2018-07-17 21:41:09 -0500

    Feat - Update. Adding comment to priority key

--- src/priorityQueue.java
+++ src/priorityQueue.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class priorityQueue {
+
+    // priority queue can be ascending and descending type.
+    // min key having the highest priority   - ascending
+    // max key having the highest priority   - descending
+
+    // mai priority key operations
+    // insert (kye, data) - inserts data with key to the priority key. Elements are ordered based on key
+    // deletemin/deletemax - Remove and return the element with the largest/smallest key
+    // getMin/Getmax - Return the element with the smallest/largest key without deleting it
+
+    
 }
