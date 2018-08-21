76b8d892d112981b9638f6d9ddf4b041f168c7a0
Biplab Kumar Das <dasbiplabtu@gmail.com>
2018-08-20 21:15:22 -0500

    Feat - New Feature. Adding redundant code

---
+++ src/redundantBraces.java
@@ -0,0 +1,37 @@
+import java.util.Stack;
+
+public class redundantBraces {
+
+
+    // ((a + b)) has redundant braces so answer will be 1
+    // (a + (a + b)) doesn't have have any redundant braces so answer will be 0
+
+    public boolean checkRedundancy(String str) {
+        Stack<Character> stack = new Stack<>();
+        for(char ch : str.toCharArray()) {
+            if(ch == ')') {
+                if(stack.isEmpty()) return false;
+                if(stack.peek() == '(') return true;
+                int elementInside = 0;
+                while(stack.pop() != '(') {
+                    elementInside++;
+                    stack.pop();
+                }
+                stack.pop();
+                if(elementInside < 2) {
+                    return 1;
+                }
+
+            }else{
+                stack.push(ch);
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        String str = "((a+ b))";
+        redundantBraces rs = new redundantBraces();
+        System.out.println(rs.checkRedundancy(str));
+    }
+}
