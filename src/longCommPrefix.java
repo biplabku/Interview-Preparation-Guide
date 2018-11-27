96b5aa362caa18d1db47299ccbc5b0904c3fdd98
Biplab kumar das <dasbiplabtu@gmail.com>
2018-11-26 18:59:27 -0600

    Feat - New Feature. Adding final implementation for finding the common prefix from a list of strings

--- src/longCommPrefix.java
+++ src/longCommPrefix.java
@@ -2,6 +2,25 @@ import java.util.ArrayList;
 
 public class longCommPrefix {
 
+    // completing the implementation for finding the long common prefix among the list of strings
+    // Comparing it with the first string everytime for all the strings
+    // Time complexity - O(N2)
+    // 
+    public  String longCommPrefix1(ArrayList<String> str) {
+        StringBuilder finalR = new StringBuilder();
+        for(int i = 0; i < str.size(); i++) {
+            for(int j = i; j < str.size(); j++) {
+                String first = str.get(0);
+                String second = str.get(j);
+                if(first.charAt(i) != second.charAt(i)) {
+                    return finalR.toString();
+                }
+            }
+            finalR.append(str.get(i).charAt(i));
+        }
+        return finalR.toString();
+    }
+
     public  String longCommPrefix(ArrayList<String> str) {
         int count = 0;
         int k = 0;
@@ -15,7 +34,7 @@ public class longCommPrefix {
                 if(str.get(i).charAt(i) == str.get(j).charAt(i)) {
                     count = j;
                     continue;
-                    
+
                 }else {
                     break;
                 }
@@ -33,10 +52,10 @@ public class longCommPrefix {
     public static void main(String[] args) {
         longCommPrefix ms   = new longCommPrefix();
         ArrayList<String> list = new ArrayList<>();
-        list.add("abcd");
-        list.add("abcd");
-        list.add("abgh");
-        System.out.println(ms.longCommPrefix(list));
+        list.add("qbcd");
+        list.add("acde");
+        list.add("abch");
+        System.out.println(ms.longCommPrefix1(list));
 
     }
 
