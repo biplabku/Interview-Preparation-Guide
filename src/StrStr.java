3f322ba8967a647650fa01fb936e9cbdee7daece
Biplab kumar das <dasbiplabtu@gmail.com>
2018-11-29 21:06:01 -0600

    Feat - New Feature. Adding String o holics implementation

--- src/StrStr.java
+++ src/StrStr.java
@@ -18,16 +18,45 @@ public class StrStr {
         for(int i = 0; i < list.size(); i++) {
             max = Math.max(list.get(i).length(), max);
         }
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < max; i++) {
-            
+        int val = 0;
+        for(int i = 0; i < list.size(); i++) {
+            String str = list.get(i);
+            val = Math.max(getRotateString(str), val);
         }
+        return val;
+    }
+
+    // generate all string after each step of rotation
+    public int getRotateString(String str) {
+        String origna = str;
+        ArrayList<String> list = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        int val = 0;
+        for(int i = 0; i < str.length(); i++) {
+            sb.append(str.charAt(str.length() - 1));
+            String st = str.substring(0, str.length() - 1);
+            sb.append(st);
+            list.add(sb.toString());
+            if(sb.toString().equalsIgnoreCase(origna)) {
+                return i;
+            }
+            str = sb.toString();
+
+            sb.setLength(0);
+        }
+        return val;
     }
 
     public static void main(String[] args) {
         StrStr str = new StrStr();
         String A = "b";
         String B = "b";
-        System.out.println(str.getStrStr(A, B));
+        ArrayList<String> list = new ArrayList<>();
+        list.add("a");
+        list.add("ababa");
+        list.add("aba");
+        //System.out.println(str.getStrStr(A, B));
+        // System.out.println(str.getRotateString("ABC"));
+        System.out.println(str.stringOHolics(list));
     }
 }
