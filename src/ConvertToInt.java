d006a22104dc57c055f33c43b1266f1869909ad8
Biplab kumar das <dasbiplabtu@gmail.com>
2018-12-04 19:14:33 -0600

    Feat - Update. Adding the later implementation

--- src/ConvertToInt.java
+++ src/ConvertToInt.java
@@ -1,23 +1,49 @@
 public class ConvertToInt {
 
     public int converToInt(final String str){
-        if(str == null) {
+        if(str == null || str.length() == 0) {
             return 0;
         }
-        int result = 0;
         StringBuilder sb = new StringBuilder();
         for(int i = 0; i < str.length(); i++) {
+            int val = Integer.valueOf(str.charAt(i));
             if(str.charAt(i) == ' ') {
                 break;
-            }else if(Integer.valueOf(str.charAt(i)) > 46 || Integer.valueOf(str.charAt(i)) < 57) {
+            }else if(val > 47 && val < 58) {
+                sb.append(str.charAt(i));
+            }else if(str.charAt(i) == '+'){
+                continue;
+            }else if(str.charAt(i) == '-') {
                 sb.append(str.charAt(i));
             }
+            else {
+                break;
+            }
         }
+        int len = sb.toString().length();
+        if(len == 0) {
+            return 0;
+        }
+        if(sb.toString().charAt(0) == '-') {
+            if(sb.length() == 1) {
+                return 0;
+            }
+            if(Double.valueOf(sb.toString().substring(1, sb.length())) > Integer.MAX_VALUE) {
+                return -Integer.MAX_VALUE;
+            }else {
+                return -Integer.valueOf(sb.toString().substring(1, sb.length()));
+            }
+        }
+        if(Double.valueOf(sb.toString()) > Integer.MAX_VALUE) {
+            return Integer.MAX_VALUE;
+        }
+
+        return Integer.valueOf(sb.toString());
     }
 
     public static void main(String[] args) {
         ConvertToInt conve = new ConvertToInt();
-        String str = "";
+        String str = "-54332872018247709407 4 54";
         System.out.println(conve.converToInt(str));
     }
 
