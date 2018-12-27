a8ebcdff88898cb7e2e83da079e59c86120ee9f4
Biplab kumar das <dasbiplabtu@gmail.com>
2018-12-27 10:58:03 -0600

    Feat - New Feature. Completing the stringoholics interview bit implementation

--- src/stringOholics.java
+++ src/stringOholics.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class stringOholics {
 
-
+    final int M = (int) 1e9+7;
     //////////// QUESTION /////////////
     //       https://www.interviewbit.com/problems/stringoholics/
     //////////// END /////////////
@@ -62,12 +62,126 @@ public class stringOholics {
     // equal.
 
 
+    void updateLcmMap(Map<Integer, Integer> m, Integer num){
+
+        int i = 2;
+
+        while(i<=num && i > 1){
+            int count = 0;
+
+            while(num % i == 0){
+                count++;
+                num /= i;
+            }
+
+            if(count == 0){
+                i++;
+                continue;
+            }
+
+            if(m.containsKey(i)){
+                int v = m.get(i);
+                if(v < count){
+                    m.put(i,count);
+                }
+            }
+            else{
+                m.put(i,count);
+            }
+
+            i++;
+        }
+    }
+
+    long getLcm(ArrayList<Integer> lens){
+
+        Map<Integer, Integer> m = new HashMap<>();
+
+        for(Integer num : lens){
+            updateLcmMap(m, num);
+        }
+
+        long prod = 1;
+        for(Map.Entry<Integer, Integer> entry : m.entrySet()){
+
+            int k = entry.getKey();
+            int v = entry.getValue();
+
+            long p = pow(k,v) % M;
+
+            prod = (prod * p) % M;
+        }
+
+        return prod % M;
+    }
+
+    public int solve(ArrayList<String> list) {
+        ArrayList<Integer> lens = new ArrayList<>();
+
+        for(String str:list) {
+            int maxLen = maxSubstring(str);
+            int n = str.length();
+
+            if(n % (n - maxLen) == 0) {
+                n -= maxLen;
+            }
+
+            long sum = 0;
+            int i = 1;
+            do{
+                sum += i;
+                i++;
+            }while(sum %((long)n) != 0L);
+            lens.add(i -1);
+        }
+        long lcm = getLcm(lens)%M;
+
+        return (int)lcm %M;
+    }
+
+    public long pow(long a, long p) {
+        long ans = 1;
+        while(p > 0) {
+            if(p % 2L == 1L) {
+                ans = (ans*a)%M;
+            }
+            a = (a*a) % M;
+            p /= 2;
+        }
+        return ans%M;
+    }
+
+    public int maxSubstring(String str) {
+        int[] lps = new int[str.length()];
+        lps[0] = 0;
+        int len = 0;
+        int n = str.length();
+        int i = 1;
+        int max = 0;
+        while(i < n) {
+            if(str.charAt(i) == str.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+                max = Math.max(max, len);
+            }else {
+                if(len == 0) {
+                    lps[i] = 0;
+                    i++;
+                }else {
+                    len = lps[len - 1];
+                }
+            }
+        }
+        return max;
+    }
 
 
     public static void main(String[] args) {
         stringOholics ss = new stringOholics();
         String str = "abcd";
-        ArrayList<String> list = new ArrayList<>(Arrays.asList("abcd", "abc", "bda"));
-        System.out.println(ss.solveString(list));
+        ArrayList<String> list = new ArrayList<>(Arrays.asList("a", "ababa", "aba"));
+        //System.out.println(ss.solveString(list));
+        System.out.println(ss.solve(list));
     }
 }
