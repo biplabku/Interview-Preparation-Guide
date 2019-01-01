5b2da48e8a1125701879e5b84b0ae927b399d619
Biplab kumar das <dasbiplabtu@gmail.com>
2019-01-01 12:52:02 -0600

    Feat - New Feature. Adding initial implementation for rat in a maze code

--- src/ratInMaze.java
+++ src/ratInMaze.java
@@ -3,7 +3,12 @@ import java.util.Stack;
 
 public class ratInMaze {
 
-
+    public int N = 3;
+    int[][] helper = {
+            {0, 0, 0},
+            {0, 0, 0},
+            {0, 0, 0}
+    };
     // Let say we first want to see if there is a path at all instead of finding a path to the goal
     public boolean findAPath(int[][] arr, int x, int y) {
         // breadth first search or depth first search
@@ -13,6 +18,7 @@ public class ratInMaze {
                 {false, false, false},
                 {false, false, false},
         };
+
         Stack<rowColsNode> thestack = new Stack<>();
         rowColsNode node = new rowColsNode(0,0);
         thestack.push(node);
@@ -58,14 +64,60 @@ public class ratInMaze {
         }
     }
 
+    // starts from point 0,0 and check whether there exists a path to n-1, n-1
+    public boolean findRoute(int[][] maze) {
+
+        boolean value = checkRoute(maze, 0, 0, helper);
+        if(value == false) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean checkRoute(int[][] maze, int x, int y, int[][] helper) {
+        if(x == N-1 && y == N-1 ) {
+            helper[x][y] = 1;
+            return true;
+        }
+
+        if(isValid(maze, x, y)) {
+            helper[x][y] = 1;
+
+            // go down
+            if(checkRoute(maze, x+1, y, helper)) {
+                return true;
+            }
+            // go right
+            if(checkRoute(maze, x, y+1, helper)) {
+                return true;
+            }
+            // go up
+            if(checkRoute(maze, x-1, y, helper )) {
+                return true;
+            }
+            // go left
+            if(checkRoute(maze, x, y-1, helper)) {
+                return true;
+            }
+            helper[x][y] = 0;
+            return false;
+        }
+        return false;
+    }
+
+    public boolean isValid(int[][] maze, int x, int y) {
+        return (x >=0 && x < N && y >= 0 && y < N && maze[x][y] == 0);
+
+    }
+
 
     public static void main(String[] args) {
         int[][] arr ={
                 {0,0,1},
                 {1,0,1},
-                {1,0,0},
+                {1,0,1},
         };
         ratInMaze ms = new ratInMaze();
-        System.out.println(ms.findAPath(arr, 0,0));
+        System.out.println(ms.findRoute(arr));
     }
 }
