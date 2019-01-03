fb6413086befe2896547eeeb39ba56515e8b567b
Biplab kumar das <dasbiplabtu@gmail.com>
2019-01-02 18:18:46 -0600

    Feat - New Feature. Adding comparable implementation to the interval class

--- src/Interval.java
+++ src/Interval.java
@@ -1,10 +1,20 @@
-public class Interval {
+public class Interval implements Comparable<Interval>{
     public int startTime;
     public int endTime;
 
     public Interval(int start, int end) {
         startTime = start;
         endTime = end;
+    }
+
+    @Override
+    public int compareTo(Interval interval) {
+        if(this.startTime < interval.startTime) {
+            return -1;
+        }else if(this.startTime == interval.startTime) {
+            return 0;
+        }
+        return 1;
     }
 
 }
