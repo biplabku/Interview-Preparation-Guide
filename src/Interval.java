6ca0b275e987ab8f879d8a8edc4e1d86eaee4da8
Biplab kumar das <dasbiplabtu@gmail.com>
2019-01-03 21:07:30 -0600

    Feat - New Feature. Experimenting with linklist code

--- src/Interval.java
+++ src/Interval.java
@@ -20,22 +20,4 @@ public class Interval implements Comparable<Interval>{
         return 1;
     }
 
-    public LinkNode add2Numbers(LinkNode l1, LinkNode l2) {
-        LinkNode result = new LinkNode();
-        int sum =0;
-        int carry = 0;
-        while(l1 != null && l2 != null) {
-            sum = l1.data + l2.data + carry;
-            if(sum >= 10) {
-                sum = sum % 2;
-                carry = sum / 2;
-            }
-            result.data = sum;
-            result = result.next;
-            l1 = l1.next;
-            l2 = l2.next;
-        }
-
-        
-    }
 }
--- src/LinkList.java
+++ src/LinkList.java
@@ -30,5 +30,59 @@ public class LinkList {
             cur = cur.next;
         }
     }
+
+
+    public LinkNode add2Numbers(LinkNode l1, LinkNode l2) {
+        LinkNode result = null;
+        int sum =0;
+        int carry = 0;
+        LinkNode prev = null;
+        LinkNode nxt = null;
+        while(l1 != null && l2 != null) {
+            sum = l1.data + l2.data + carry;
+            if(sum >= 10) {
+                sum = sum % 2;
+                carry = sum / 2;
+            }
+            LinkNode temp = new LinkNode(sum);
+            result = temp;
+            result = result.next;
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+        // setting the head of the linklist is needed to resolve this issue
+        if(carry != 0) {
+            result.next = new LinkNode(carry);
+        }
+
+        return head.next;
+    }
+
+    public void reverseLinkList(LinkList l1) {
+        LinkNode cur = l1.head;
+        LinkNode prev = null;
+        LinkNode nxt ;
+        while(cur != null) {
+            nxt = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = nxt;
+        }
+        l1.head = prev;
+        l1.display();
+    }
+
+    public static  void main(String[] args) {
+        LinkList list = new LinkList();
+        list.add(2);
+        list.add(4);
+        list.add(3);
+        LinkList list2 = new LinkList();
+        list2.add(5);
+        list2.add(6);
+        list2.add(4);
+        // System.out.println(ls.add2Numbers(a1, a2));
+        list.reverseLinkList(list);
+    }
 }
 
