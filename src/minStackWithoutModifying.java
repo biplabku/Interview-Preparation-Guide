ab0b563b51e4639eab3fca803fca5c59b9c62c0c
Biplab kumar das <biplabkumardas@MacBook-Pro.attlocal.net>
2019-01-06 12:00:21 -0600

    Feat - New Feature. Adding minimum stack next implementation for finding the minimum value

---
+++ src/minStackWithoutModifying.java
@@ -0,0 +1,84 @@
+public class minStackWithoutModifying {
+
+    int curSize = 0;
+    int size = 4;
+    int[] array ;
+    int min ;
+    public minStackWithoutModifying(int val) {
+        array = new int[size];
+        min = Integer.MAX_VALUE;
+        curSize = 0;
+    }
+
+    public boolean isEmpty() {
+        if(curSize == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isFull() {
+        if(curSize == size - 1) {
+            return true;
+        }
+        return false;
+    }
+
+    public void push(int x) {
+        if(!isEmpty() || !isFull()) {        // if its not empty
+            array[curSize] = x;
+            if(min > x) {
+                min = Math.min(x, min);
+            }
+            curSize++;
+        }else {
+            System.out.println("Stack is full");
+        }
+    }
+
+    public int pop() {
+        if(!isEmpty()) {
+            int val = array[curSize - 1];
+            curSize--;
+            updateMin();
+            return val;
+        }
+        return-1;
+    }
+
+    public void updateMin() {
+        min = array[curSize - 1];
+        for(int i =0; i < curSize; i++) {
+            min = Math.min(min, array[i]);
+        }
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public int peek() {
+        if(!isEmpty()) {
+            return array[curSize];
+        }
+        return -1;
+    }
+
+    public void display() {
+        for(int i =0; i < curSize; i++) {
+            System.out.println(array[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        minStackWithoutModifying ms = new minStackWithoutModifying(4);
+        ms.push(1);
+        ms.push(2);
+        ms.push(3);
+        ms.push(0);
+        ms.display();
+        ms.pop();
+        ms.pop();
+        System.out.println(ms.getMin());
+    }
+}
