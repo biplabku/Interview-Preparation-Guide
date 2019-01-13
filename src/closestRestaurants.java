9bea55a581a64afc45e9c1c4d203561045f08c98
dasbiplabtu@gmail.com <biplabkumardas@Biplabs-MBP.attlocal.net>
2019-01-13 10:48:28 -0600

    Feat - New Feature. Adding the final initial implementation

--- src/closestRestaurants.java
+++ src/closestRestaurants.java
@@ -5,10 +5,12 @@ import java.util.List;
 public class closestRestaurants {
 
 
-    public List<Integer> ClosesNeighbors(List<List<Integer>> list, int N, int K) {
+    public List<Integer> ClosesNeighbors(List<List<Integer>> list, int K) {
         int[] array = new int[list.size()];
         for(int i = 0; i < list.size(); i++) {
             List<Integer> temp = list.get(i);
+            System.out.println(temp.get(0) + " " + temp.get(1));
+
             int dist = calcDistance(temp.get(0), temp.get(1)) ;
             array[i] = dist;
         }
@@ -27,6 +29,31 @@ public class closestRestaurants {
     public static void main(String[] args) {
         List<List<Integer>> list = new ArrayList<>();
         List<Integer> temp = new ArrayList<>();
+        temp.add(1);
+        temp.add(2);
+        List<Integer> temp1 = new ArrayList<>();
 
+        temp1.add(2);
+        temp1.add(4);
+
+        List<Integer> temp2 = new ArrayList<>();
+        temp2.add(0);
+        temp2.add(1);
+
+        List<Integer> temp3 = new ArrayList<>();
+        temp3.add(3);
+        temp3.add(4);
+
+        List<Integer> temp4 = new ArrayList<>();
+        temp4.add(5);
+        temp4.add(6);
+
+        list.add(temp);
+        list.add(temp1);
+        list.add(temp2);
+        list.add(temp3);
+        list.add(temp4);
+        closestRestaurants ms = new closestRestaurants();
+        System.out.println(ms.ClosesNeighbors(list, 4));
     }
 }
