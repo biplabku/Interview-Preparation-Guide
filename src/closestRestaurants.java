9f95ba51e130a39840b9dddfa3fd5167fb109f9a
Biplab Kumar Das <dasbiplabtu@gmail.com>
2019-01-13 19:30:26 -0600

    Feat - New Feature. Adding max stack implementation

--- src/closestRestaurants.java
+++ src/closestRestaurants.java
@@ -1,36 +1,71 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class closestRestaurants {
 
 
-    public List<Integer> ClosesNeighbors(List<List<Integer>> list, int K) {
+    public List<List<Integer>> ClosesNeighbors(List<List<Integer>> list, int K) {
         int[] array = new int[list.size()];
+        HashMap<Integer, List<Integer>> hmap = new HashMap<>();
         for(int i = 0; i < list.size(); i++) {
             List<Integer> temp = list.get(i);
-            System.out.println(temp.get(0) + " " + temp.get(1));
-
             int dist = calcDistance(temp.get(0), temp.get(1)) ;
-            array[i] = dist;
+            hmap.put(dist, temp);
         }
-        List<Integer> result = new ArrayList<>();
-        Arrays.sort(array);
-        for(int i = 0; i < K; i++) {
-            result.add(array[i]);
+
+        Map<Integer, List<Integer>> sortMap = new TreeMap<>(hmap);
+        Set set2 = sortMap.entrySet();
+        Iterator iter = set2.iterator();
+        List<List<Integer>> result = new ArrayList<>();
+        while(iter.hasNext()) {
+            Map.Entry pair = (Map.Entry)iter.next();
+            List<Integer> temp = (List<Integer>)pair.getValue();
+            result.add(temp);
         }
         return result;
     }
 
     public int calcDistance(int x, int y) {
-        return (int)Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+        return (int)Math.sqrt((0 - x) * (0 - x) + (0 - y) * (0 - y));
+    }
+
+    public int mexHeightStack(int[] array1, int[] array2, int[] array3) {
+        cumulativeSum(array1);
+        cumulativeSum(array2);
+        cumulativeSum(array3);
+        int index1 = array2.length - 1;
+        int index2 = array3.length - 1;
+        int max = 0;
+        for(int i = array1.length - 1; i >= 0; i--) {
+            int el = array1[i];
+            while(el != array2[index1] && index1 > 0) {
+                index1--;
+            }
+            while(el != array3[index2] && index2 > 0) {
+                index2--;
+            }
+            if(index1 < 0 && index2 < 0 || index1 == 0 && index2 >= 0 ) {
+                index1  = array2.length - 1;
+                index2 = array3.length - 1;
+                continue;
+            }else if(index1 >= 0 && index2 >= 0) {
+                max = el;
+            }
+        }
+        return max;
+    }
+
+    public void cumulativeSum(int[] array) {
+        for(int i =1; i < array.length; i++) {
+            array[i] = array[i - 1] + array[i];
+        }
     }
 
     public static void main(String[] args) {
         List<List<Integer>> list = new ArrayList<>();
+        /*
         List<Integer> temp = new ArrayList<>();
-        temp.add(1);
-        temp.add(2);
+        temp.add(8);
+        temp.add(9);
         List<Integer> temp1 = new ArrayList<>();
 
         temp1.add(2);
@@ -53,7 +88,12 @@ public class closestRestaurants {
         list.add(temp2);
         list.add(temp3);
         list.add(temp4);
-        closestRestaurants ms = new closestRestaurants();
         System.out.println(ms.ClosesNeighbors(list, 4));
+        */
+        closestRestaurants ms = new closestRestaurants();
+        int[] array = new int[]{1,1,3,1};
+        int[] array1 = new int[]{2,2,1,4};
+        int[] array2 = new int[]{5,1,2};
+        System.out.println(ms.mexHeightStack(array, array1, array2));
     }
 }
