34978b2727358c6103ebf410f7f8d47ef2b877bd
Biplab Kumar Das <dasbiplabtu@gmail.com>
2019-01-19 13:33:01 -0600

    Feat - Update. Saving the prior changes

--- src/MapTutorials.java
+++ src/MapTutorials.java
@@ -1,6 +1,4 @@
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
 
 public class MapTutorials {
 
@@ -9,15 +7,30 @@ public class MapTutorials {
     // based on both hash table and linked list to enhance the functionality of the
     // hashmap
     public void linkedHashMap( ) {
-        LinkedHashMap<Integer, String> hmap = new LinkedHashMap<>();
-        for(int i = 0; i < 4; i++) {
-            hmap.put(5, "apple");
-            hmap.put(1, "mango");
-            hmap.put(1, "tango");
-        }
+        HashMap<Integer, String> hmap = new HashMap<>();
+        hmap.put(5, "apple");
+        hmap.put(0, "mango");
+        hmap.put(-1, "tango");
+        hmap.put(9, "apple");
+        hmap.put(10, "mango");
+        hmap.put(8, "tango");
         Iterator iter = hmap.entrySet().iterator();
         while(iter.hasNext()) {
             Map.Entry pair = (Map.Entry) iter.next();
+            System.out.println((int)pair.getKey());
+        }
+        System.out.println("done");
+
+        LinkedHashMap<Integer, String> tmap = new LinkedHashMap<>();
+        tmap.put(5, "apple");
+        tmap.put(0, "mango");
+        tmap.put(-1, "tango");
+        tmap.put(9, "apple");
+        tmap.put(10, "mango");
+        tmap.put(8, "tango");
+        Iterator iter1 = tmap.entrySet().iterator();
+        while(iter1.hasNext()) {
+            Map.Entry pair = (Map.Entry) iter1.next();
             System.out.println((int)pair.getKey());
         }
     }
