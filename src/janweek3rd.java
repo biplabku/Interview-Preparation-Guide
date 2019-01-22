19848481aa19a89f52df368843f7cadd0aa176cf
Biplab Kumar Das <dasbiplabtu@gmail.com>
2019-01-21 19:26:53 -0600

    Feat - New Feature. Adding shortest to char initial implementation

---
+++ src/janweek3rd.java
@@ -0,0 +1,13 @@
+public class janweek3rd {
+
+    public int[] shortestToChar(String str, String ch) {
+        int[] array = new int[str.length()];
+
+        return array;
+    }
+
+
+    public static void main(String[] args) {
+        janweek3rd js = new janweek3rd();
+    }
+}
