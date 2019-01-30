051b3af267e8fbc1ce428d0e4262f3998785bb47
Biplab Kumar Das <dasbiplabtu@gmail.com>
2019-01-29 20:11:18 -0600

    Feat - Update. Adding the nth minimum code implementation

--- src/nthMinElement.java
+++ src/nthMinElement.java
@@ -1,7 +1,9 @@
+import java.util.Arrays;
+
 public class nthMinElement {
 
 
-    public int minElemeny(int[] array) {
+    public int minElement(int[] array) {
         int min = array[0];
         for(int i = 1; i < array.length; i++) {
             min = Math.min(min, array[i]);
@@ -23,6 +25,37 @@ public class nthMinElement {
         return secondMin;
     }
 
+    public int nthMinElement(int[] array, int n) {
+        int start = 0;
+        int end = array.length - 2;
+        int[] newArray = new int[array.length ];
+        int index = 0;
+        for(int i = 1; i < array.length; i++) {
+            if(array[i] < array[index]) {
+                newArray[start] = array[i];
+                start++;
+            }else {
+                newArray[end] = array[i];
+                end--;
+            }
+        }
+        System.out.println(Arrays.toString(newArray));
+        if(n > start) {
+            return nthMinElement(Arrays.copyOfRange(newArray, start + 1 , newArray.length),
+                    n -  start);
+        }else if(n < start) {
+            return nthMinElement(Arrays.copyOfRange(newArray, 0, start), n);
+        }else{
+            return array[start];
+        }
+    }
+
+    public void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
 
     public void display(int[] array) {
         for(int i = 0; i < array.length; i++) {
@@ -33,7 +66,7 @@ public class nthMinElement {
 
     public static void main(String[] args) {
         nthMinElement ms = new nthMinElement();
-        int[] array = {2,3,4,5,6, 0,1};
-        System.out.println(ms.secondMinElement(array));
+        int[] array = {5,6,3,15,11};
+        System.out.println(ms.nthMinElement(array, 2));
     }
 }
