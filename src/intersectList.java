1c7fc2300a9a2f28b8f42d35061b54f5180f648f
Biplab Kumar Das <dasbiplabtu@gmail.com>
2019-02-21 19:01:30 -0600

    Feat - Update. Adding the brute force solution to russian doll implementation

--- src/intersectList.java
+++ src/intersectList.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 public class intersectList {
 
@@ -34,6 +32,74 @@ public class intersectList {
                 }
             }
         }
+        return max;
+    }
+
+    public int getRussianDollImplemen(int[][] list) {
+        int max = 0;
+        for(int i = 0; i < list.length; i++) {
+            List<Integer> temp1 = new ArrayList<>();
+            temp1.add(list[i][0]);
+            temp1.add(list[i][1]);
+            Stack<List<Integer>> theStack = new Stack<>();
+            theStack.push(temp1);
+            for(int j = 0; j < list.length; j++) {
+                if(i != j) {
+                    List<Integer> temp2 = new ArrayList<>();
+                    temp2.add(list[j][0]);
+                    temp2.add(list[j][1]);
+                    if(temp1.get(0) < temp2.get(0) && temp1.get(1) < temp2.get(1)) {
+                        if(!theStack.isEmpty()) {
+                            Stack<List<Integer>> tmpStack = new Stack<>();
+                            List<Integer> cur = theStack.peek();
+                            while(cur.get(0) > temp2.get(0) && cur.get(1) > temp2.get(1) && !theStack.isEmpty()) {
+                                tmpStack.push(theStack.pop());
+                            }
+                            if(theStack.isEmpty() || theStack.peek().get(0) < temp2.get(0) && theStack.peek().get(1) < temp2.get(1) ) {
+                                theStack.push(temp2);
+                            }
+                            while(!tmpStack.isEmpty()) {
+                                theStack.push(tmpStack.pop());
+                            }
+                        }else {
+                            theStack.push(temp2);
+                        }
+                    }
+                    max = Math.max(max, theStack.size());
+                }
+            }
+        }
+        return max;
+    }
+
+    public int bruteRussianDoll(int[][] envelopes) {
+        if(envelopes == null || envelopes.length == 0) {
+            return 0;
+        }
+
+        Arrays.sort(envelopes, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if(o1[0] != o2[0]) {
+                    return o1[0] - o2[0];
+                }else {
+                    return o1[1] - o2[1];
+                }
+            }
+        });
+
+        int max = 1;
+        int[] arr = new int[envelopes.length];
+        for(int i = 0; i < envelopes.length; i++){
+            arr[i] = 1;
+            for(int j = i - 1; j >= 0; j--){
+                if(envelopes[i][0] > envelopes[j][0] && envelopes[i][1] > envelopes[j][1]){
+                    arr[i] = Math.max(arr[i], arr[j]+1);
+                }
+            }
+            max = Math.max(max, arr[i]);
+        }
+
         return max;
     }
 
@@ -122,7 +188,9 @@ public class intersectList {
         list.add(t3);
         list.add(t4);
 
+        int[][] array = {{2,100}, {3,200}, {4,300}, {5,500}, {5,400}, {5, 250}, {6,370}, {6,360}, {7,380}};
+
         intersectList ls = new intersectList();
-        System.out.println(ls.getRussianDollImplementation(list));
+        System.out.println(ls.bruteRussianDoll(array));
     }
 }
