8d391288ab4513a6f3d5d917478c866f68f12287
Biplab Kumar Das <biplabdas@Biplabs-MBP.attlocal.net>
2019-07-05 21:16:26 -0500

    Feat - New Feature. Adding combine string based on the map

--- src/srcToDestination.java
+++ src/srcToDestination.java
@@ -1,4 +1,72 @@
+import java.util.*;
+import java.util.Collections;
+import java.util.Comparator;
+
 public class srcToDestination {
+
+    // str = hello
+    // list = "he", "el",
+    // if there is a match then start with break and end with a break and if there
+    // is no match then save that character
+    public String combineString(String str, List<String> dictionary) {
+        StringBuilder sb = new StringBuilder();
+        String openBr = "<br>";
+        String closeBr = "</br>";
+
+        ArrayList<Points> list = new ArrayList<>();
+
+        for(int i = 0; i < dictionary.size(); i++) {
+            String s = dictionary.get(i);
+            int y = str.indexOf(s);
+            if(y != -1) {
+                Points p = new Points(y, y + s.length());
+                list.add(p);
+            }
+        }
+
+        // sort the elements based on the first occurence;
+        Collections.sort(list, new Comparator<Points>() {
+            @Override
+            public int compare(Points o1, Points o2) {
+                return o1.x - o2.x;
+            }
+        });
+
+        // how to merge them based on the sorted values
+        sb.append(openBr);
+        if(list.get(0).x == 0) {
+            sb.append(str.substring(0, list.get(0).y));
+        }else {
+            sb.append(str.substring(0,list.get(0).x));
+        }
+        sb.append(closeBr);
+        for(int i = 1; i < list.size(); i++) {
+            Points prev = list.get(i - 1);
+            Points next = list.get(i);
+            if(prev.y  + 1 >= next.x) {
+                String s = str.substring(next.x, next.y);
+                sb.append(openBr);
+                sb.append(s);
+                sb.append(closeBr);
+            }else {
+                String s = str.substring(prev.y + 1, next.x - 1);
+                sb.append(s);
+            }
+        }
+        if(list.get(list.size() -1).y != str.length()) {
+            sb.append(str.substring(list.get(list.size() - 1).y));
+        }
+        return sb.toString();
+    }
+
+    class Points{
+        int x;
+        int y;
+        public Points(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
 
     public static int getTotalDistanceFromSourceToDestination (int[][] grid) {
         int rows = grid.length;
@@ -35,14 +103,75 @@ public class srcToDestination {
     }
 
     public static boolean isValidMove(int[][] grid, boolean[][] visited, int i, int j) {
-        return (i >=0 && i < grid.length && j >= 0 && j < grid[0].length && (grid[i][j] == 1 || grid[i][j] ==9) && visited[i][j] == false);
+        return (i >=0 && i < grid.length && j >= 0 && j < grid[0].length && (grid[i][j] == 1 ||
+                grid[i][j] ==9) && visited[i][j] == false);
 
+    }
+
+    static class DNode{
+        char c;
+        HashMap<Character, DNode> children = new HashMap<>();
+        boolean isEndOfWord = false;
+
+        public DNode() {
+        }
+
+        public DNode(char ch) {
+            c = ch;
+        }
+    }
+
+    static DNode root;
+
+    public srcToDestination() {
+        root = new DNode();
+    }
+
+    public static boolean wordBreak(String str, List<String> wordDict) {
+        HashMap<Character, DNode> childrens = root.children;
+        for(int i = 0; i < wordDict.size(); i++) {
+            String s = wordDict.get(i);
+            for(int j = 0; j < s.length(); j++) {
+                char ch = s.charAt(j);
+                DNode t;
+                if(childrens.containsKey(ch)) {
+                    t = childrens.get(ch);
+                }else {
+                    t = new DNode(ch);
+                    childrens.put(ch, t);
+                }
+                childrens = t.children;
+                if(i == s.length()-1){
+                    t.isEndOfWord = true;
+                }
+            }
+        }
+
+        DNode child = null;
+        for(int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if(childrens.containsKey(ch)) {
+                child = childrens.get(ch);
+                if(child.isEndOfWord == true) {
+                    child = null;
+                }
+            }else {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
         int[][] grid ={{ 1, 0, 1},
                         {1, 0, 1 },
                         {1, 1, 9 }};
-        System.out.println(getTotalDistanceFromSourceToDestination(grid));
+        srcToDestination ss = new srcToDestination();
+        List<String> list = new ArrayList<>();
+        list.add("apple");
+        list.add("pen");
+        String str = "applepena";
+        //System.out.println(wordBreak(str, list));
+        System.out.println(ss.combineString(str, list));
     }
 }
